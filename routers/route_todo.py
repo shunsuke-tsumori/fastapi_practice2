9ca24681f29dbefb786d7266a41eecacde6194f7
Shunsuke Tsumori <tsumori.shunsuke@gmail.com>
2022-08-21 14:16:32 +0900

    add update and delete endpoints

--- routers/route_todo.py
+++ routers/route_todo.py
@@ -3,8 +3,8 @@ from typing import List
 from fastapi import APIRouter, Request, Response, status, HTTPException
 from fastapi.encoders import jsonable_encoder
 
-from database import db_create_todo, db_get_tools, db_get_single_todo
-from schemas import Todo, TodoBody
+from database import db_create_todo, db_get_tools, db_get_single_todo, db_update_todo, db_delete_todo
+from schemas import Todo, TodoBody, SuccessMsg
 
 router = APIRouter()
 
@@ -31,3 +31,20 @@ async def get_single_tool(id: str):
     if res:
         return res
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Task of ID:{id} doesn't exist")
+
+
+@router.put("/api/todo/{id}", response_model=Todo)
+async def update_todo(id: str, data: TodoBody):
+    todo = jsonable_encoder(data)
+    res = await db_update_todo(id, todo)
+    if res:
+        return res
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Update task failed")
+
+
+@router.delete("/api/todo/{id}", response_model=SuccessMsg)
+async def delete_todo(id: str):
+    res = await db_delete_todo(id)
+    if res:
+        return {"message": "Successfully deleted"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Delete task failed")
